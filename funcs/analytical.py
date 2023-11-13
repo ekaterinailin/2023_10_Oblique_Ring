6ddc96aec68fe023e95fb937081151e75624b6c0
ekaterinailin <ekaterina.ilin@posteo.de>
2023-11-13 12:39:51 +0100

    auroral ring class works

--- funcs/analytical.py
+++ funcs/analytical.py
@@ -7,7 +7,122 @@ Ekaterina Ilin 2023 -- MIT Licencse
 Functions for the analytical model.
 """
 
+from numpy import pi, sin, cos, sqrt, abs, arcsin, max, digitize, bincount
+
 import numpy as np
+
+
+def get_analytical_spectral_line(phi, i_rot, i_mag, latitude, alpha, bins, omega, Rstar, norm=10):
+    """Calculate the broadened spectral line of the infinitesimally narrow
+    auroral ring.
+    
+    Parameters
+    ----------
+    phi : array
+        The phase angles of the ring in rad. From 0 to 2 pi.
+    i_rot : float
+        The rotation inclination in rad.
+    i_mag : float
+        The magnetic obliquity in rad.
+    latitude : float
+        The latitude of the ring in rad.
+    alpha : float
+        The rotational phase of the star in rad.
+    bins : int
+        The number of velocity bins to use for the spectral line.
+    omega : float
+        The rotation rate of the star in rad / day.
+    Rstar : float
+        The radius of the star in solar radii.
+    norm : float
+        The normalization factor for the flux. Default is 10.
+
+    Returns
+    -------
+    flux : array
+        The flux of the spectral line.
+    """
+
+    # get the parameters for the positions
+    A, B, C = x_params(alpha, i_rot=i_rot, i_mag=i_mag, latitude=pi/2 - latitude)
+
+    # get the parameters for the velocities
+    X, Y, Z = vx_params(alpha, i_rot=i_rot, i_mag=i_mag, latitude=pi/2 - latitude)
+
+    # get the velocities and positions
+    v = v_phi(phi, X, Y, Z)
+    x = x_phi(phi, A, B, C)
+
+    # get the flux
+    flux = flux_at_x_vx(v, x, X, Y, Z, norm=norm)
+
+    # mask the flux
+    flux[x<0] = 0
+
+    # # convert to stellar radii / s and then to km/s
+    v = v * omega / 86400 * Rstar * 695700. 
+
+
+    # argsort the velocities
+    sort = np.argsort(v)
+
+    # sort the velocities and fluxes
+    v = v[sort]
+    flux = flux[sort]
+
+    # define the bins
+    digitized = digitize(v, bins)
+
+    # flux_ = np.zeros_like(bins[:-1])
+
+    # calculate the flux in each bin
+    # print(np.unique(digitized))
+    flux_ = bincount(digitized, weights=flux, minlength=len(bins) - 1)
+
+    # normalize the flux
+    flux_ = flux_ / max(flux_)
+
+    return flux_
+
+
+def x_phi(phi, A, B, C):
+    """Calculate the x position of the ring at a given phase angle phi.
+    
+    Parameters
+    ----------
+    phi : array
+        The phase angles of the ring in rad. From 0 to 2 pi.
+    A : float
+        The A parameter of the ring position.
+    B : float
+        The B parameter of the ring position.
+    C : float       
+        The C parameter of the ring position.
+    """
+    return B * sin(phi) + C * cos(phi) + A
+
+
+def v_phi(phi, X, Y, Z):
+    """Calculate the x-velocity of the ring at a given phase angle phi.
+    
+    Parameters
+    ----------
+    phi : array
+        The phase angles of the ring in rad. From 0 to 2 pi.
+    X : float
+        The X parameter of the ring velocity.
+    Y : float
+        The Y parameter of the ring velocity.
+    Z : float
+        The Z parameter of the ring velocity.
+
+    Returns
+    -------
+    v : array
+        The x-velocity of the ring at the given phase angles.
+    """
+    return Y * sin(phi) + Z * cos(phi) + X
+
 
 def x_params(alpha, i_rot, i_mag, latitude):
     """Calculate the parameters for the x-position of the ring.
@@ -34,11 +149,11 @@ def x_params(alpha, i_rot, i_mag, latitude):
     """
 
     # define shorthands for various trig functions
-    sa, ca = np.sin(alpha), np.cos(alpha)
-    si, ci = np.sin(i_rot), np.cos(i_rot)
-    st, ct = np.sin(latitude), np.cos(latitude)
-    sip, cip = np.sin(i_rot + i_mag), np.cos(i_rot + i_mag)
-    Ca = 1 - np.cos(alpha)
+    sa, ca = sin(alpha), cos(alpha)
+    si, ci = sin(i_rot), cos(i_rot)
+    st, ct = sin(latitude), cos(latitude)
+    sip, cip = sin(i_rot + i_mag), cos(i_rot + i_mag)
+    Ca = 1 - cos(alpha)
 
     # calculate the parameters
     A = ct * sip * (ca + si**2 * Ca) + ct * cip *ci * si * Ca
@@ -72,10 +187,10 @@ def vx_params(alpha, i_rot, i_mag, latitude):
     """
     
     # define shorthands for various trig functions
-    sa, ca = np.sin(alpha), np.cos(alpha)
-    si, ci = np.sin(i_rot), np.cos(i_rot)
-    st, ct = np.sin(latitude), np.cos(latitude)
-    sip, cip = np.sin(i_rot + i_mag), np.cos(i_rot + i_mag)
+    sa, ca = sin(alpha), cos(alpha)
+    si, ci = sin(i_rot), cos(i_rot)
+    st, ct = sin(latitude), cos(latitude)
+    sip, cip = sin(i_rot + i_mag), cos(i_rot + i_mag)
 
     # calculate the parameters
     X = -sa * (sip * ct - ct * sip * si**2 - ct * cip * si * ci)
@@ -83,3 +198,38 @@ def vx_params(alpha, i_rot, i_mag, latitude):
     Z = -ca * ci * st
 
     return X, Y, Z
+
+
+def flux_at_x_vx(vx, x, X, Y, Z, norm=10):
+    """Calculate the flux of the ring at a given x position and x-velocity.
+    Use the derivative of the phi(vx) function to calculate the flux, then 
+    multiply by the geometrical foreshortening factor.
+    
+    Parameters
+    ----------
+    vx : array
+        The x-velocity of the ring.
+    x : array
+        The x position of the ring.
+    X : float
+        The X parameter of the ring velocity.
+    Y : float
+        The Y parameter of the ring velocity.
+    Z : float
+        The Z parameter of the ring velocity.
+    norm : float
+        The normalisation factor to deal with the 
+        singularity when denominator becomes zero.
+
+    Returns
+    -------
+    flux : array
+        The flux of the ring at the given positions and velocities.
+    """
+    foreshortening = cos(pi/2 - arcsin(x)) 
+    flux = 1 / sqrt(-(vx - X)**2 + Y**2 + Z**2 + norm*max(abs(vx-X)))
+    
+    return flux * foreshortening
+
+
+
--- funcs/tests/test_analytical.py
+++ funcs/tests/test_analytical.py
@@ -7,11 +7,52 @@ Ekaterina Ilin 2023 -- MIT Licencse
 Tests for the analytical model functions.
 """
 
-from ..analytical import x_params, vx_params
+from ..analytical import x_params, vx_params, x_phi, v_phi, flux_at_x_vx, get_analytical_spectral_line
 
 import numpy as np
+import pytest
 
-# write a unit test function for x_params
+def test_get_analytical_spectral_line():
+    """Test the get_analytical_spectral_line function.
+    """
+    # define the parameters
+    i_rot = 0
+    i_mag = 0
+    latitude = 0
+    alpha = 0
+    bins = np.linspace(-20, 20, 100)
+    omega = 2 * np.pi / 3
+    Rstar = 1
+
+    # get the phase angles
+    phi = np.linspace(0, 2*np.pi, 1000)
+
+    # get the flux
+    flux = get_analytical_spectral_line(phi, i_rot, i_mag, latitude, alpha, bins, omega, Rstar, norm=.1)
+
+    # assert that the flux is a numpy array
+    assert isinstance(flux, np.ndarray)
+
+    # assert that the flux is between 0 and 1
+    assert np.all(flux >= 0)
+    assert np.all(flux <= 1)
+
+    # assert that the flux is normalized
+    assert np.isclose(np.max(flux), 1)
+
+    # assert that the flux is not all zeros
+    assert not np.allclose(flux, np.zeros_like(flux))
+
+    # assert that the flux is finite
+    assert np.all(np.isfinite(flux))
+
+
+    # maximum flux should be at the edges where the velocity is also highest
+    binmids = (bins[1:] + bins[:-1]) / 2
+    maxv = 2 * np.pi * Rstar * 695700 / (3 * 86400)
+    assert binmids[np.argmax(flux)] / maxv == pytest.approx(1, rel=1e-2)
+
+
 def test_x_params():
     """Test the x_params function.
     """
@@ -85,3 +126,73 @@ def test_vx_params():
     X_exp = 0
     Y_exp = 1
     Z_exp = 0
+
+
+def test_x_phi():
+    """Test the x_phi function."""
+    phi = np.linspace(0, 2*np.pi, 1000)
+    A = 1
+    B = 0
+    C = 0
+    x = x_phi(phi, A, B, C)
+    assert np.allclose(x, 1)
+
+    A = 0
+    B = 1
+    C = 0
+    x = x_phi(phi, A, B, C)
+    assert np.allclose(x, np.sin(phi))
+
+    A = 0
+    B = 0
+    C = 1
+    x = x_phi(phi, A, B, C)
+    assert np.allclose(x, np.cos(phi))
+
+def test_v_phi():
+    """Test the v_phi function."""
+    phi = np.linspace(0, 2*np.pi, 1000)
+    X = 1
+    Y = 0
+    Z = 0
+    v = v_phi(phi, X, Y, Z)
+    assert np.allclose(v, 1)
+
+    X = 0
+    Y = 1
+    Z = 0
+    v = v_phi(phi, X, Y, Z)
+    assert np.allclose(v, np.sin(phi))
+
+    X = 0
+    Y = 0
+    Z = 1
+    v = v_phi(phi, X, Y, Z)
+    assert np.allclose(v, np.cos(phi))
+
+
+
+def test_flux_at_x_vx():
+    """Test the flux_at_x_vx function.
+    """
+    vx = np.linspace(-10, 10, 100)
+    x = np.linspace(0, 1, 100)
+    X = 0
+    Y = 0
+    Z = 0
+    
+    flux = flux_at_x_vx(vx, x, X, Y, Z)
+    
+    # flux is inf at 10 and -10
+    assert ~np.isfinite(flux[-1])
+    assert ~np.isfinite(flux[0])
+
+    # if you feed nan, you get nan back
+    flux = flux_at_x_vx(np.nan, np.nan, X, Y, Z)
+
+    assert np.isnan(flux)
+
+    # if you feed x>1 you get nan back
+    flux = flux_at_x_vx(20, 1.1, X, Y, Z) 
+
+    assert np.isnan(flux)
