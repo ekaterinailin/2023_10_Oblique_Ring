fix flux normalization

funcs/analytical.py
@@ -79,8 +79,9 @@ def get_analytical_spectral_line(phi, i_rot, i_mag, latitude, alpha, bins, omega
     # print(np.unique(digitized))
     flux_ = bincount(digitized, weights=flux, minlength=len(bins) - 1)
 
-    # normalize the flux
-    flux_ = flux_ / max(flux_)
+    # normalize the flux unless it is all zeros
+    if max(flux_) != 0:
+        flux_ = flux_ / max(flux_)
 
     return flux_
 

funcs/numerical.py
@@ -46,6 +46,7 @@ def numerical_spectral_line(alpha, x, y, z, z_rot, omega, Rstar, bins, dalpha=.1
     # print("numbins", _)
 
     # normalize the flux
-    flux = flux / np.max(flux)
+    if max(flux) != 0:
+        flux = flux / np.max(flux)
 
     return flux